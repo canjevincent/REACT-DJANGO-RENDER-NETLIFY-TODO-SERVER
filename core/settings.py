5b7e19cccb77bade03bc29efbe06a301fa19be37
canjevincent <120143083+canjevincent@users.noreply.github.com>
2023-10-25 11:32:02 +0800

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG","False").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -57,6 +57,7 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     'django-react-aws-netlify-todo-dev-app.netlify.app',
+    'https://django-react-aws-netlify-todo-dev-app.netlify.app',
 ]
 
 REST_FRAMEWORK = {
